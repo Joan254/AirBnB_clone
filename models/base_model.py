8211df70fdf43b74cabe711ea0cb361bbad1199a
Parrot Live user <user@localhost.localdomain>
2023-11-08 14:05:20 +0000

    creating the base class

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+"""Defining a base class for the whole project"""
+
+
+class BaseModel():
+    """This it the base that defines all common atributes for other clases"""
+    def __init__(self):
+        """Initializing an object with these instance attribute
+           Args:
+               self.id (int) : The unique id of the instance
+               current_time : Generates the time 
+               self.created_time: Time created
+               self.updated_time: The upated time
+        """
+        self.id = str(uuid.uuid4())
+
+        current_time = datetime.now()
+        self.created_at = current_time
+        self.updated_at = current_time
+
+    def save(self):
+        """A public instance method to update the update_at with thecurrent time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def __str__():
+        """Custom string representation: should print [<class name>] (<self.id>) <self.__dict__>"""
+        return "[{}] ({}) {}".format(self.__class__.__name, self.id, self.__dict__)
