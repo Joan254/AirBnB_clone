e34de317643033a7522b15339c9683e14a8c5ead
Joan254 <joanotieno1998@gmail.com>
2023-11-08 12:41:25 +0000

    BaseModel updated

--- models/base_model.py
+++ models/base_model.py
@@ -6,26 +6,37 @@ from datetime import datetime
 
 class BaseModel():
     """This it the base that defines all common atributes for other clases"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializing an object with these instance attribute
            Args:
                self.id (int) : The unique id of the instance
-               current_time : Generates the time 
+               current_time : Generates the time
                self.created_time: Time created
                self.updated_time: The upated time
         """
-        self.id = str(uuid.uuid4())
-
-        current_time = datetime.now()
-        self.created_at = current_time
-        self.updated_at = current_time
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if "created_at" == key or "updated_at" == key:
+                    self.__dict__[key] = datetime.strptime(value, date_format)
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            current_time = datetime.now()
+            self.created_at = current_time
+            self.updated_at = current_time
 
     def save(self):
-        """A public instance method to update the update_at with thecurrent time"""
+        """A public instance method to update the update_at
+        with the current time.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """Returns a dictionary containing all keys/values
+        of __dict__ of the instance.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
@@ -33,5 +44,7 @@ class BaseModel():
         return obj_dict
 
     def __str__(self):
-        """Custom string representation: should print [<class name>] (<self.id>) <self.__dict__>"""
+        """Custom string representation:
+        should print [<class name>] (<self.id>) <self.__dict__>
+        """
         return f"[BaseModel] ({self.id}) {self.__dict__}"
